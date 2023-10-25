0f447e502380861c1305e08e477721152c78e25d
darkensteve <118660632+darkensteve@users.noreply.github.com>
2023-10-25 18:25:57 +0800

    Create NumbersPrintln.java

---
+++ PRELIM/NumbersPrintln.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+  public class NumberPrintln
+    {
+      public static void main(String [] args)
+      {
+        int billingDate = 5;
+        System.out.println("Bills are sent on day ");
+        System.out.print(billingDate);
+        System.out.println(" of the month");
+        System.out.println("Next bill: October " + billingDate);
+      }
+    }   
