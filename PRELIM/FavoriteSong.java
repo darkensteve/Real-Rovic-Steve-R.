145d0c2af34e4279c4e9446ecc27a0d39c44f2f7
darkensteve <118660632+darkensteve@users.noreply.github.com>
2023-10-25 18:17:32 +0800

    Create FavoriteSong.java

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,10 @@
+public class FavoriteSong
+ {
+   public static void main(String [] args)
+     {
+       System.out.println("I see her online");
+       System.out.println("All the time");
+       System.out.println("I'm trying not to stare down there");
+       System.out.println("While she talks about her tough time");
+       }
+       }
